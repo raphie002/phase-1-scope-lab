f1e4edcfbd0256383986f01be54d7519a95c6ed7
Raphael <raphael.njoroge02@gmail.com>
2025-06-10 20:28:04 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function checkcustomerName() {
+    console.log(customerName)
+}
+
+function upperCaseCustomerName() {
+    customerName = "BOB"
+    console.log(customerName)
+}
+
+var bestCustomer
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    throw ("Assignment to constant variable.")
+}
